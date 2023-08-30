f4b4d5941946ec428c0b7e9704ff937af5d80b1a
Denner Matheus <108595078+dnrmth@users.noreply.github.com>
2023-08-30 09:56:59 -0300

    Add files via upload

---
+++ src/dbConnection.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class dbConnection {
+    public static void main(String[] args) {
+
+        String dbURL = "jdbc:mysql://localhost:3306/world";
+        String dbName = "root";
+        String dbPassword = "21091998";
+
+        try {
+            Connection connection = DriverManager.getConnection(dbURL, dbName, dbPassword);
+            String query = "SELECT c.Code, c.Name, cl.Language "
+                    + "FROM Country c "
+                    + "JOIN CountryLanguage cl ON c.Code = cl.CountryCode "
+                    + "where cl.IsOfficial = 'T'"
+                    + "LIMIT 10";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.println("Country: " + rs.getString("Name") + ". Official language: " + rs.getString("Language"));
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar com o banco de dados: " + e.getMessage());
+        }
+    }
+}
